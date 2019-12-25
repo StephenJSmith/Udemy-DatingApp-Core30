f6db64f51ec52926ed10b15dc78da6b890cb2721
Stephen Smith <stephensmith_aus@yahoo.com.au>
2019-12-26 08:35:15 +1100

    End of Section 3

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -1,48 +1,57 @@
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
   [ApiController]
-    [Route("api/[controller]")]
-    public class ValuesController : ControllerBase
+  [Route("api/[controller]")]
+  [Authorize]
+
+  public class ValuesController : ControllerBase
+  {
+    private readonly DataContext _context;
+
+    public ValuesController(DataContext context)
     {
-        private readonly DataContext _context;
-
-        public ValuesController(DataContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetValues() {
-            var values = await _context.Values.ToListAsync();
-
-            return Ok(values);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetValue(int id) {
-            var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
-
-            return Ok(value);
-        }
-
-        [HttpPost]
-        public void Post([FromBody] string value) {
-
-        }
-
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value) {
-
-        }
-
-        [HttpDelete("{id}")]
-        public void Delete(int id) {
-
-        }
+      _context = context;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetValues()
+    {
+      var values = await _context.Values.ToListAsync();
+
+      return Ok(values);
+    }
+
+    [HttpGet("{id}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetValue(int id)
+    {
+      var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+
+      return Ok(value);
+    }
+
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+
+    }
+
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+
+    }
+
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+
+    }
+  }
 }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -5,47 +5,66 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace DatingApp.API
 {
   public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            var cnnString = Configuration.GetConnectionString("DefaultConnection");
-            services.AddDbContext<DataContext>(x => x.UseSqlite(cnnString));
-            services.AddControllers();
-            services.AddCors();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            // app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      var cnnString = Configuration.GetConnectionString("DefaultConnection");
+      services.AddDbContext<DataContext>(x => x.UseSqlite(cnnString));
+      services.AddControllers();
+      services.AddCors();
+      services.AddScoped<IAuthRepository, AuthRepository>();
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+          .AddJwtBearer(options =>
+          {
+            var tokenValue = Configuration.GetSection("AppSettings:Token").Value;
+            var encodedTokenValue = Encoding.ASCII.GetBytes(tokenValue);
+
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+              ValidateIssuerSigningKey = true,
+              IssuerSigningKey = new SymmetricSecurityKey(encodedTokenValue),
+              ValidateIssuer = false,
+              ValidateAudience = false
+            };
+          });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      // app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
