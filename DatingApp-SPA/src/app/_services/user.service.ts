4f3f7255bdf7a73b20dbee14c9a482ea260f2cf2
Stephen Smith <stephensmith_aus@yahoo.com.au>
2019-12-28 17:12:22 +1100

    End of Section 9

---
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    const url = this.baseUrl + 'users';
+
+    return this.http.get<User[]>(url);
+  }
+
+  getUser(id): Observable<User> {
+    const url = this.baseUrl + 'users/' + id;
+
+    return this.http.get<User>(url);
+  }
+}
